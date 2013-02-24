3d1e96f1587e2f50d3bb2a6702a3a186335f449c
Ethan Steinberg <ethan.steinberg@gmail.com>
2013-02-23 20:17:27 -0800

    Oh, more stuff

--- twitterfeed/views.py
+++ twitterfeed/views.py
@@ -1,7 +1,11 @@
 from . import app
 
-from flask import render_template
+from flask import render_template;
 
 @app.route('/')
-def boo():
-	return render_template('index.html');
+def index():
+	return render_template('index.html')
+
+@app.route('/teaminfo')
+def teaminfo():
+	return render_template('teaminfo.html')
