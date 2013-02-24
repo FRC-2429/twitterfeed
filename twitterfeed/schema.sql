13560da79cb83ca8794ff05402e3eb6d87c7d69e
Ethan Steinberg <ethan.steinberg@gmail.com>
2013-02-24 13:02:25 -0800

    Schema update

--- twitterfeed/schema.sql
+++ twitterfeed/schema.sql
@@ -8,3 +8,28 @@ CREATE TABLE statustable
 );
 
 INSERT INTO statustable (currently_being_modified,last_id_recieved)  VALUES (FALSE, 0);
+
+DROP TABLE IF EXISTS teams;
+CREATE TABLE teams
+(
+	id serial PRIMARY KEY,
+	team_number integer PRIMARY KEY
+
+);
+
+
+DROP TABLE IF EXISTS events;
+CREATE TABLE events
+(
+	id serial PRIMARY KEY,
+	year integer NOT NULL,
+	name text NOT NULL
+);
+
+DROP TABLE IF EXISTS event_team_relationships;
+CREATE TABLE event_team_relationships
+(
+	id serial PRIMARY KEY,
+	event_id integer REFERENCES events (id),
+	team_id integer REFERENCES teams (id)
+);
